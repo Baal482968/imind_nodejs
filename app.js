0a2bb90482fd73eff4542a44ce44c014b49c962e
BaalWu <baal482968@gmail.com>
2017-10-23 18:31:49 +0800

    add basic code in routes

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const authRoutes = require('./routes/auth-routes');
+const passportSetup = require('./config/passport-setup');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+const app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
